31fe5767c68e77e505f2f6b5b31042397d968c4c
gygounet@gmail.com <gygounet@gmail.com@0667feea-a708-11de-a118-c7387b1e4f66>
2009-10-07 08:05:35 +0000

    Prueba a medias, 2 hrs peleandome con esta pantallita xD

--- TP1/Primitivas Graficas/testClippingLB.cpp
+++ TP1/Primitivas Graficas/testClippingLB.cpp
@@ -23,3 +23,84 @@
 //	return 0;
 //}
 //
+
+#include "GL/glut.h"
+#include <stdlib.h>
+
+#include <iostream> //TODO sacar
+
+#include "Poligono.h"
+#include "Linea/linea.h"
+#include "liang-barsky.h"
+
+#define ANCHO	800
+#define ALTO	600
+
+char caption[]="Solsticio Nerd";
+
+void reshape(int w, int h)
+{
+   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   gluOrtho2D(0.0, (GLdouble)w, (GLdouble)h, 0.0);
+}
+
+void init(void)
+{
+}
+
+
+void display(void)
+{
+
+	glClear(GL_COLOR_BUFFER_BIT);
+   	glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
+    Poligono cuadrado;
+	cuadrado.agregarVertice(20.0,40.0);
+	cuadrado.agregarVertice(20.0,60.0);
+	cuadrado.agregarVertice(40.0,60.0);
+	cuadrado.agregarVertice(40.0,40.0);
+	
+	Poligono* triangulo = new Poligono();;
+	triangulo->agregarVertice(35.0,5.0);
+	triangulo->agregarVertice(5.0,60.0);
+	triangulo->agregarVertice(75.0,60.0);
+    
+    ClippingPoligonos clippeador;
+    Poligono* clip = new Poligono();
+    clip = clippeador.clippingLB(40,60,20,40,triangulo,0);
+//    clip = clippeador.clippingLB(10,200,20,200,clip,1);
+//    clip = clippeador.clippingLB(10,200,20,200,clip,2);  
+    glBegin(GL_POINTS);
+
+    cuadrado.dibujarContorno(true);
+
+	clip->dibujarContorno(true);
+
+
+
+	glEnd();
+
+	///
+  	glutSwapBuffers();
+	///
+}
+
+
+int main(int argc, char** argv)
+{
+   glutInit(&argc, argv);
+   glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
+   glutInitWindowSize (ANCHO, ALTO);
+   glutInitWindowPosition (100, 100);
+   glutCreateWindow (caption);
+   init ();
+   glutDisplayFunc(display);
+   glutReshapeFunc(reshape);
+   glutMainLoop();
+
+   return 0;
+}
