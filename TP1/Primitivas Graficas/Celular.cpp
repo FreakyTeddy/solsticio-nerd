4b49b5c24fb3be12489b85699849d1f97e69f846
gygounet@gmail.com <gygounet@gmail.com@0667feea-a708-11de-a118-c7387b1e4f66>
2009-10-08 18:59:32 +0000

    Modificacion pedida por vero

--- TP1/Primitivas Graficas/Celular.cpp
+++ TP1/Primitivas Graficas/Celular.cpp
@@ -18,30 +18,30 @@ Celular::~Celular() {
 
 void Celular::crearCarcasa() {
 
-	cel.agregarVertice(25,0);
-	cel.agregarVertice(17,4);
-	cel.agregarVertice(15,10);
-	cel.agregarVertice(15,65);
-	cel.agregarVertice(17,71);
-	cel.agregarVertice(28,75);
-	cel.agregarVertice(25,80);
-	cel.agregarVertice(17,84);
-	cel.agregarVertice(15,90);
-	cel.agregarVertice(15,150);
-	cel.agregarVertice(17,156);
-	cel.agregarVertice(25,160);
-	cel.agregarVertice(75,160);
-	cel.agregarVertice(83,156);
-	cel.agregarVertice(85,150);
-	cel.agregarVertice(85,90);
-	cel.agregarVertice(82,84);
-	cel.agregarVertice(74,80);
-	cel.agregarVertice(72,75);
-	cel.agregarVertice(82,71);
-	cel.agregarVertice(85,65);
-	cel.agregarVertice(85,10);
-	cel.agregarVertice(82,4);
-	cel.agregarVertice(75,0);
+	cel.agregarVertice(55,0);
+	cel.agregarVertice(47,4);
+	cel.agregarVertice(45,10);
+	cel.agregarVertice(45,65);
+	cel.agregarVertice(47,71);
+	cel.agregarVertice(58,75);
+	cel.agregarVertice(55,80);
+	cel.agregarVertice(47,84);
+	cel.agregarVertice(45,90);
+	cel.agregarVertice(45,150);
+	cel.agregarVertice(47,156);
+	cel.agregarVertice(55,160);
+	cel.agregarVertice(105,160);
+	cel.agregarVertice(113,156);
+	cel.agregarVertice(115,150);
+	cel.agregarVertice(115,90);
+	cel.agregarVertice(112,84);
+	cel.agregarVertice(104,80);
+	cel.agregarVertice(102,75);
+	cel.agregarVertice(112,71);
+	cel.agregarVertice(115,65);
+	cel.agregarVertice(115,10);
+	cel.agregarVertice(112,4);
+	cel.agregarVertice(105,0);
 	cel.setDimensiones(160,160);
 
 }
@@ -49,48 +49,48 @@ void Celular::crearCarcasa() {
 void Celular::crearSombras() {
 
 	//sombra arriba
-	sombra[0].agregarVertice(19,155);
-	sombra[0].agregarVertice(17,152);
-	sombra[0].agregarVertice(16,145);
-	sombra[0].agregarVertice(16,90);
-	sombra[0].agregarVertice(19,85);
-	sombra[0].agregarVertice(21,84);
-	sombra[0].agregarVertice(24,82);
-	sombra[0].agregarVertice(21,86);
-	sombra[0].agregarVertice(19,90);
+	sombra[0].agregarVertice(49,155);
+	sombra[0].agregarVertice(47,152);
+	sombra[0].agregarVertice(46,145);
+	sombra[0].agregarVertice(46,90);
+	sombra[0].agregarVertice(49,85);
+	sombra[0].agregarVertice(51,84);
+	sombra[0].agregarVertice(54,82);
+	sombra[0].agregarVertice(51,86);
+	sombra[0].agregarVertice(49,90);
 
 	//sombra abajo
-	sombra[1].agregarVertice(19,70);
-	sombra[1].agregarVertice(17,69);
-	sombra[1].agregarVertice(16,65);
-	sombra[1].agregarVertice(16,10);
-	sombra[1].agregarVertice(19,5);
-	sombra[1].agregarVertice(21,4);
-	sombra[1].agregarVertice(24,2);
-	sombra[1].agregarVertice(21,6);
-	sombra[1].agregarVertice(19,10);
+	sombra[1].agregarVertice(49,70);
+	sombra[1].agregarVertice(47,69);
+	sombra[1].agregarVertice(46,65);
+	sombra[1].agregarVertice(46,10);
+	sombra[1].agregarVertice(49,5);
+	sombra[1].agregarVertice(51,4);
+	sombra[1].agregarVertice(54,2);
+	sombra[1].agregarVertice(51,6);
+	sombra[1].agregarVertice(49,10);
 
 	//sombra clara abajo
-	sombra[2].agregarVertice(22,71);
-	sombra[2].agregarVertice(19,70);
-	sombra[2].agregarVertice(19,65);
-	sombra[2].agregarVertice(19,10);
-	sombra[2].agregarVertice(22,5);
-	sombra[2].agregarVertice(24,4);
-	sombra[2].agregarVertice(27,2);
-	sombra[2].agregarVertice(24,6);
-	sombra[2].agregarVertice(22,10);
+	sombra[2].agregarVertice(52,71);
+	sombra[2].agregarVertice(49,70);
+	sombra[2].agregarVertice(49,65);
+	sombra[2].agregarVertice(49,10);
+	sombra[2].agregarVertice(52,5);
+	sombra[2].agregarVertice(54,4);
+	sombra[2].agregarVertice(57,2);
+	sombra[2].agregarVertice(54,6);
+	sombra[2].agregarVertice(52,10);
 
 	//sombra clara arriba
-	sombra[3].agregarVertice(22,157);
-	sombra[3].agregarVertice(19,154);
-	sombra[3].agregarVertice(19,145);
-	sombra[3].agregarVertice(19,90);
-	sombra[3].agregarVertice(22,85);
-	sombra[3].agregarVertice(24,84);
-	sombra[3].agregarVertice(27,82);
-	sombra[3].agregarVertice(24,86);
-	sombra[3].agregarVertice(22,90);
+	sombra[3].agregarVertice(52,157);
+	sombra[3].agregarVertice(49,154);
+	sombra[3].agregarVertice(59,145);
+	sombra[3].agregarVertice(49,90);
+	sombra[3].agregarVertice(52,85);
+	sombra[3].agregarVertice(54,84);
+	sombra[3].agregarVertice(57,82);
+	sombra[3].agregarVertice(54,86);
+	sombra[3].agregarVertice(52,90);
 
 	for (int i=0; i<4;i++)
 		sombra[i].setDimensiones(160,160);
