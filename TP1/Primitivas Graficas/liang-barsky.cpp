194a3262ee705a677e8a673b60a56cd1d4c737c7
LUCASHPANDOLFO@gmail.com <LUCASHPANDOLFO@gmail.com@0667feea-a708-11de-a118-c7387b1e4f66>
2009-10-08 13:09:49 +0000

    Sorry, aca esta el clipping. Tiene 4 pruebas, la cuarta no la pasa todavia, pero esta cerca.

--- TP1/Primitivas Graficas/liang-barsky.cpp
+++ TP1/Primitivas Graficas/liang-barsky.cpp
@@ -16,7 +16,7 @@ void ClippingPoligonos::clippingLB(int Vtop, int Vbottom, int Vleft, int Vright,
 	float dx = x1 - x0;
 	float dy = y1 - y0;
 
-	if(!dx || !dy) return;
+//	if(!dx || !dy) return;
 	float p[4];
 	float q[4];
 	p[0] = -dx;	
@@ -42,12 +42,6 @@ void ClippingPoligonos::clippingLB(int Vtop, int Vbottom, int Vleft, int Vright,
 			}
 		}
 		
-		//TODO agregar vertices
-		
-//		x0 = x0 + (t0 * dx);
-//		y0 = y0 + (t0 * dy);
-//		x1 = x0 + (t1 * dx);
-//		y1 = y0 + (t1 * dy);
 	}
 	std::cout << "Puntos antes de calcular: x0=" << x0 << " y0= " << y0 << " x1= " << x1 << " y1= " << y1 << std::endl;
 	std::cout << "parametros: t0= " << t0 << " t1= " << t1 << std::endl;
@@ -62,96 +56,163 @@ void ClippingPoligonos::clippingLB(int Vtop, int Vbottom, int Vleft, int Vright,
 	
 }
 
+Poligono* ClippingPoligonos::clippingLB(int Vtop, int Vbottom, int Vleft, int Vright, Poligono* poligono){
+     std::list<Vertice> verticesOriginales = poligono->obtenerVertices();
+     std::list<Vertice>::iterator it = verticesOriginales.begin();
+     Poligono* poligonoClippeado = new Poligono();
+     std::list<Vertice> verticesClippeados = poligonoClippeado->obtenerVertices();
+     Vertice vertice0;
+     Vertice vertice1;
+     float xo0,xo1,yo0,yo1; //pares x,y originales
+     int i = 0;
+     bool encontrado = false;
+     float x0 = (*it).x;
+     float x1 = 0;
+     float y0 = (*it).y;
+     float y1 = 0;
+
+     xo0=x0;
+     yo0=y0;
+
+     it++;
+     for(;it!=verticesOriginales.end();it++,x0=xo1,y0=yo1){
+	  xo1=x1=(*it).x;
+	  yo1=y1=(*it).y;
+
+	  if(x0 < Vleft && x1 < Vleft){
+	       continue;
+	  }
+
+
+	  std::cout << "x0: " << x0 << " y0: " << y0 << std::endl;
+	  std::cout << "x1: " << x1 << " y1: " << y1 << std::endl;
+
+	  clippingLB(Vtop,Vbottom,Vleft,Vright, x0,x1,y0,y1);
+
+	  std::cout << "x0: " << x0 << " y0: " << y0 << std::endl;
+	  std::cout << "x1: " << x1 << " y1: " << y1 << std::endl;
+
+	  std::cout << "----------------------------------\n";
+
+	  poligonoClippeado->agregarVertice(x0,y0);
+	  poligonoClippeado->agregarVertice(x1,y1);
+     }
+
+	  x1=xo0;
+	  y1=yo0;
+
+	  std::cout << "x0: " << x0 << " y0: " << y0 << std::endl;
+	  std::cout << "x1: " << x1 << " y1: " << y1 << std::endl;
+
+
+	  clippingLB(Vtop,Vbottom,Vleft,Vright, x0,x1,y0,y1);
+
+	  std::cout << "x0: " << x0 << " y0: " << y0 << std::endl;
+	  std::cout << "x1: " << x1 << " y1: " << y1 << std::endl;
+
+	  std::cout << "----------------------------------\n";
+
+	  poligonoClippeado->agregarVertice(x0,y0);
+	  poligonoClippeado->agregarVertice(x1,y1);
+
+
+	  return poligonoClippeado;
+}
+
+
+
+
+
 Poligono* ClippingPoligonos::clippingLB(int Vtop, int Vbottom, int Vleft, int Vright, Poligono* poligono, int numVerticeInicial)
 {
-	std::list<Vertice> verticesOriginales = poligono->obtenerVertices();
-	std::list<Vertice>::iterator it = verticesOriginales.begin();
-	Poligono* poligonoClippeado = new Poligono();
-	int i = 0;
-	bool encontrado = false;
-	float x0 = 0;
-	float x1 = 0;
-	float y0 = 0;
-	float y1 = 0;
-	for(it = verticesOriginales.begin(); it != verticesOriginales.end() && !encontrado; it++,i++){
-		if(i == numVerticeInicial){
-			x0 = (*it).x;
-			y0 = (*it).y;
-		}else{
-			if(i > numVerticeInicial){
-				encontrado = true;
-				x1= (*it).x;
-				y1 = (*it).y;
-			}
-		}
-	}
+     std::list<Vertice> verticesOriginales = poligono->obtenerVertices();
+     std::list<Vertice>::iterator it = verticesOriginales.begin();
+     Poligono* poligonoClippeado = new Poligono();
+     int i = 0;
+     bool encontrado = false;
+     float x0 = 0;
+     float x1 = 0;
+     float y0 = 0;
+     float y1 = 0;
+     for(it = verticesOriginales.begin(); it != verticesOriginales.end() && !encontrado; it++,i++){
+	  if(i == numVerticeInicial){
+	       x0 = (*it).x;
+	       y0 = (*it).y;
+	  }else{
+	       if(i > numVerticeInicial){
+		    encontrado = true;
+		    x1= (*it).x;
+		    y1 = (*it).y;
+	       }
+	  }
+     }
 	
-	float dx = x1 - x0;
-	float dy = y1 - y0;
+     float dx = x1 - x0;
+     float dy = y1 - y0;
 
-	if(!dx || !dy) return NULL;
-	float p[4];
-	float q[4];
-	p[0] = -dx;	
-	p[1] = dx;
-	p[2] = -dy;
-	p[3] = dy;
-	q[0] = x0 - Vleft;
-	q[1] = Vright - x0;
-	q[2] = y0 - Vbottom;
-	q[3] = Vtop - y0;
-	float t = 0;
-	float t0 = 0;
-	float t1 = 1;
+     if(!dx || !dy) return NULL;
+     float p[4];
+     float q[4];
+     p[0] = -dx;	
+     p[1] = dx;
+     p[2] = -dy;
+     p[3] = dy;
+     q[0] = x0 - Vleft;
+     q[1] = Vright - x0;
+     q[2] = y0 - Vbottom;
+     q[3] = Vtop - y0;
+     float t = 0;
+     float t0 = 0;
+     float t1 = 1;
 	
-	for(int i = 0; i < 4; i++){
-		t = q[i] / p[i];
-		std::cout << "paso: "<<i<<" t=" << t << std::endl;
-		if((t >= 0) && (t <= 1)){
-			if(p[i] < 0){
-				if(t > t0) t0 = t;
-			}else{
-				if(t < t1) t1 = t;
-			}
-		}
+     for(int i = 0; i < 4; i++){
+	  t = q[i] / p[i];
+	  std::cout << "paso: "<<i<<" t=" << t << std::endl;
+	  if((t >= 0) && (t <= 1)){
+	       if(p[i] < 0){
+		    if(t > t0) t0 = t;
+	       }else{
+		    if(t < t1) t1 = t;
+	       }
+	  }
 		
-		//TODO agregar vertices
+	  //TODO agregar vertices
 		
 //		x0 = x0 + (t0 * dx);
 //		y0 = y0 + (t0 * dy);
 //		x1 = x0 + (t1 * dx);
 //		y1 = y0 + (t1 * dy);
-	}
-	std::cout << "Puntos antes de calcular: x0=" << x0 << " y0= " << y0 << " x1= " << x1 << " y1= " << y1 << std::endl;
-	std::cout << "parametros: t0= " << t0 << " t1= " << t1 << std::endl;
-	std::cout << "deltas: dx= " << dx << " dy= " << dy << std::endl;
-	Vertice vertice0;
-	Vertice vertice1;
+     }
+     std::cout << "Puntos antes de calcular: x0=" << x0 << " y0= " << y0 << " x1= " << x1 << " y1= " << y1 << std::endl;
+     std::cout << "parametros: t0= " << t0 << " t1= " << t1 << std::endl;
+     std::cout << "deltas: dx= " << dx << " dy= " << dy << std::endl;
+     Vertice vertice0;
+     Vertice vertice1;
 	
-	vertice1.y = y0 + (t1 * dy);
-	vertice1.x = x0 + (t1 * dx);
-	vertice0.x = x0 + (t0 * dx);
-	vertice0.y = y0 + (t0 * dy);
+     vertice1.y = y0 + (t1 * dy);
+     vertice1.x = x0 + (t1 * dx);
+     vertice0.x = x0 + (t0 * dx);
+     vertice0.y = y0 + (t0 * dy);
 	
-	// agrego vertices
-	std::list<Vertice> verticesClippeados = poligonoClippeado->obtenerVertices();
-	i = 0;
-	for(it = verticesOriginales.begin() ; it != verticesOriginales.end() ; it++, i++){
-		if(i == numVerticeInicial){
-			verticesClippeados.push_back(vertice0);
+     // agrego vertices
+     std::list<Vertice> verticesClippeados = poligonoClippeado->obtenerVertices();
+     i = 0;
+     for(it = verticesOriginales.begin() ; it != verticesOriginales.end() ; it++, i++){
+	  if(i == numVerticeInicial){
+	       verticesClippeados.push_back(vertice0);
 			
-		}else{
-			if (i == numVerticeInicial+1){
-				verticesClippeados.push_back(vertice1);
-			}else{
-				verticesClippeados.push_back(*it);
-			}
-		}
-	}
+	  }else{
+	       if (i == numVerticeInicial+1){
+		    verticesClippeados.push_back(vertice1);
+	       }else{
+		    verticesClippeados.push_back(*it);
+	       }
+	  }
+     }
 
-	std::cout << "Puntos despues de calcular: x0=" << x0 << " y0= " << y0 << " x1= " << x1 << " y1= " << y1 << std::endl;
+     std::cout << "Puntos despues de calcular: x0=" << x0 << " y0= " << y0 << " x1= " << x1 << " y1= " << y1 << std::endl;
 	
-	return poligonoClippeado;
+     return poligonoClippeado;
 }
 
 
--- TP1/Primitivas Graficas/testClippingLB.cpp
+++ TP1/Primitivas Graficas/testClippingLB.cpp
@@ -1,106 +1,108 @@
-//#include <iostream>
-//#include "liang-barsky.h"
-//
-//
-////int main (int argc, char** argv){
-////	ClippingPoligonos clipper;
-////	float x0 = 30.0;
-////	float y0 = 30.0;
-////	float x1 = 65.0;
-////	float y1 = 0.0;
-////	int Vtop = 40;
-////	int Vbottom = 5;
-////	int Vleft = 5;
-////	int Vright = 70;
-////
-////	std::cout << "Linea original: x0 = " << x0 << "x1 = " << x1 << "y0 = " << y0 << "y1 = " << y1 << std::endl;
-////	std::cout << "Viewport: Vleft = " << Vleft << "Vright = " << Vright << "Vtop = " << Vtop << "Vbottom = " << Vbottom << std::endl;
-////
-////	clipper.clippingLB(Vtop,Vbottom,Vleft,Vright,x0,x1,y0,y1);
-////
-////	std::cout << "Linea clippeada: x0 = " << x0 << "x1 = " << x1 << "y0 = " << y0 << "y1 = " << y1 << std::endl;
-////
-////	return 0;
-////}
-////
-//
-//#include "GL/glut.h"
-//#include <stdlib.h>
-//
-//#include <iostream> //TODO sacar
-//
-//#include "Poligono.h"
-//#include "Linea/linea.h"
-//#include "liang-barsky.h"
-//
-//#define ANCHO	800
-//#define ALTO	600
-//
-//char caption[]="Solsticio Nerd";
-//
-//void reshape(int w, int h)
-//{
-//   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
-//   glMatrixMode(GL_PROJECTION);
-//   glLoadIdentity();
-//   gluOrtho2D(0.0, (GLdouble)w, (GLdouble)h, 0.0);
-//}
-//
-//void init(void)
-//{
-//}
-////
-////
-////void display(void)
-////{
-////
-////	glClear(GL_COLOR_BUFFER_BIT);
-////   	glMatrixMode(GL_MODELVIEW);
-////        glLoadIdentity();
-////
-////    Poligono cuadrado;
-////	cuadrado.agregarVertice(20.0,40.0);
-////	cuadrado.agregarVertice(20.0,60.0);
-////	cuadrado.agregarVertice(40.0,60.0);
-////	cuadrado.agregarVertice(40.0,40.0);
-////	
-////	Poligono* triangulo = new Poligono();;
-////	triangulo->agregarVertice(35.0,5.0);
-////	triangulo->agregarVertice(5.0,60.0);
-////	triangulo->agregarVertice(75.0,60.0);
-////    
-////    ClippingPoligonos clippeador;
-////    Poligono* clip = new Poligono();
-////    clip = clippeador.clippingLB(40,60,20,40,triangulo,0);
-//////    clip = clippeador.clippingLB(10,200,20,200,clip,1);
-//////    clip = clippeador.clippingLB(10,200,20,200,clip,2);  
-////    glBegin(GL_POINTS);
-////
-////    cuadrado.dibujarContorno(true);
-////
-////	clip->dibujarContorno(true);
-////
-////
-////
-////	glEnd();
-////
-////	///
-////  	glutSwapBuffers();
-////	///
-////}
-////
-////
-////int main(int argc, char** argv)
-////{
-////   glutInit(&argc, argv);
-////   glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
-////   glutInitWindowSize (ANCHO, ALTO);
-////   glutInitWindowPosition (100, 100);
-////   glutCreateWindow (caption);
-////   init ();
-////   glutDisplayFunc(display);
-////   glutReshapeFunc(reshape);
-////   glutMainLoop();
-////
-////   return 0;
-////}
+
+#include "GL/glut.h"
+#include <stdlib.h>
+
+#include <iostream> //TODO sacar
+
+#include "Poligono.h"
+#include "Linea/linea.h"
+#include "liang-barsky.h"
+
+#define ANCHO	800
+#define ALTO	600
+
+char caption[]="Solsticio Nerd";
+
+void reshape(int w, int h)
+{
+  glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(0.0, (GLdouble)w, (GLdouble)h, 0.0);
+}
+
+void init(void)
+{
+}
+
+
+void display(void)
+{
+
+	glClear(GL_COLOR_BUFFER_BIT);
+  	glMatrixMode(GL_MODELVIEW);
+       glLoadIdentity();
+
+   Poligono cuadrado;
+	cuadrado.agregarVertice(20.0,40.0);
+	cuadrado.agregarVertice(20.0,60.0);
+	cuadrado.agregarVertice(40.0,60.0);
+	cuadrado.agregarVertice(40.0,40.0);
+	
+	Poligono* triangulo = new Poligono();;
+	// triangulo->agregarVertice(35.0,54.0);
+	// triangulo->agregarVertice(32,45);
+	// triangulo->agregarVertice(16,57);
+
+
+	// triangulo->agregarVertice(70.0,54.0);
+	// triangulo->agregarVertice(32,45);
+	// triangulo->agregarVertice(16,57);
+
+	// triangulo->agregarVertice(35.0,54.0);
+	// triangulo->agregarVertice(3,45);
+	// triangulo->agregarVertice(16,57);
+
+	triangulo->agregarVertice(35.0,54.0);
+	triangulo->agregarVertice(3,45);
+	triangulo->agregarVertice(15,90);
+
+
+        MatrizTrans2D matTrans;
+   Vertice fixed;  /* escala del celular */
+        fixed.x = 80;
+        fixed.y = 80;
+
+matTrans.scale2D(1,1, fixed);
+        matTrans.translate2D(0,0);
+
+   
+   ClippingPoligonos clippeador;
+   Poligono* clip = new Poligono();
+   clip = clippeador.clippingLB(60,40,20,40,triangulo);
+   // clip = clippeador.clippingLB(10,200,20,200,clip,1);
+   // clip = clippeador.clippingLB(10,200,20,200,clip,2);  
+   glBegin(GL_POINTS);
+
+   glColor3f(0,1.0,0);
+   cuadrado.dibujarContorno(true, matTrans);
+   glColor3f(1.0,1.0,0);
+   triangulo->dibujarContorno(true, matTrans);
+
+   glColor3f(1.0,0,0);
+   clip->dibujarContorno(true, matTrans);
+
+
+
+	glEnd();
+
+	///
+ 	glutSwapBuffers();
+	///
+}
+
+
+int main(int argc, char** argv)
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
+  glutInitWindowSize (ANCHO, ALTO);
+  glutInitWindowPosition (100, 100);
+  glutCreateWindow (caption);
+  init ();
+  glutDisplayFunc(display);
+  glutReshapeFunc(reshape);
+  glutMainLoop();
+
+  return 0;
+}
